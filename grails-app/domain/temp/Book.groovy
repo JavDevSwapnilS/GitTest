dd9e5ea058a62cfd70a7dc61c8665b4fd14c70df
JevDevSwapnilS <sapyashah@gmail.com>
2013-02-01 13:28:22 +0200

    first commit

---
+++ grails-app/domain/temp/Book.groovy
@@ -0,0 +1,8 @@
+package temp
+
+class Book {
+
+    String title
+    Date releaseDate
+    String author
+}
---
+++ test/unit/temp/BookTestControllerTests.groovy
@@ -0,0 +1,17 @@
+package temp
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(BookTestController)
+class BookTestControllerTests {
+
+    void testSomething() {
+        fail "Implement me"
+    }
+}
