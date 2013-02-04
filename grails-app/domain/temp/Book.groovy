ea1973d43b095c06206e24a107475bbddce30c3d
Hrishikesh Madur <hrishikesh.madur@gmail.com>
2013-02-04 10:11:20 +0200

    Added Constraints

--- grails-app/domain/temp/Book.groovy
+++ grails-app/domain/temp/Book.groovy
@@ -5,4 +5,10 @@ class Book {
     String title
     Date releaseDate
     String author
+
+    static constraints = {
+        title nullable: true, maxSize: 50
+        releaseDate nullable: true
+        author nullable: true
+    }
 }
