460151400c373c9e461db176d1a1ab48fbcdb607
Hrishikesh Madur <hrishikesh.madur@gmail.com>
2013-02-04 11:16:29 +0200

    Added New Fields

--- grails-app/domain/temp/Book.groovy
+++ grails-app/domain/temp/Book.groovy
@@ -6,11 +6,13 @@ class Book {
     Date releaseDate
     String author
     String distributor
+    int noOfPages = 0
 
     static constraints = {
         title nullable: true, maxSize: 50
         releaseDate nullable: true
         author nullable: true
         distributor nullable: true
+        noOfPages nullable: true
     }
 }
